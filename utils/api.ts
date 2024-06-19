f0bb39ee377cc731661225337933b7be1bf434fc
journey-ad <journey.adc@gmail.com>
2024-06-19 22:59:50 +0800

    :bug: Fix docker network

--- utils/api.ts
+++ utils/api.ts
@@ -4,22 +4,28 @@ export function getBaseUrl() {
     return process.env.NEXT_PUBLIC_BASE_URL;
   }
 
-  // Fallback to using localhost with a dynamic port
-  const port = process.env.PORT || 3000;
+  // Fallback to localhost with port 3000
+  const host = process.env.NEXT_PUBLIC_HOST || "localhost";
+  const port = parseInt(process.env.PORT || "3000", 10);
 
-  return `http://localhost:${port}`;
+  return `http://${host}:${port}`;
 }
 
-async function apiFetch(endpoint: string, options?: RequestInit) {
-  const baseUrl = getBaseUrl();
-  const url = `${baseUrl}${endpoint}`;
-  const response = await fetch(url, options);
+async function apiFetch(endpoint: string, options?: RequestInit): Promise<any> {
+  try {
+    const baseUrl = getBaseUrl();
+    const url = `${baseUrl}${endpoint}`;
+    const response = await fetch(url, options);
 
-  if (!response.ok) {
-    throw new Error(`Network response was not ok: ${response.statusText}`);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error: any) {
+    console.error(`Failed to fetch: ${error.message}`);
+    throw error;
   }
-
-  return response.json();
 }
 
 export default apiFetch;
